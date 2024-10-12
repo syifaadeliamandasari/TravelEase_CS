6aa59c136d4ab1cce05ab6908f9ecc633a8774f7
syifa adelia mandasari <syifaadeliamandasari@gmail.com>
2024-10-12 22:26:59 +0700

    update

--- resources/views/Ticket/chat.blade.php
+++ resources/views/Ticket/chat.blade.php
@@ -4,58 +4,73 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"/>
-    <link href='https://unpkg.com/boxicons@latest/css/boxicons.min.css' rel='stylesheet'>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <title>Ticket</title>
     <link href="{{ asset('css/chat.css') }}" rel="stylesheet">
 </head>
 <body>
-    <nav class="sidebar">
-        <div class="menu_content">
-            <ul class="menu_items">
-                <div class="logo_item">
-                    <span class="icon1">
-                        <i class="bx bx-compass"></i>
-                    </span>
-                    <h1 class="teks2">TravelEase</h1>
+    <div class="container">
+        <nav class="sidebar">
+            <div class="menu_content">
+                <div class="sidebar-2">
+                    <i class="bx bx-chevron-left icons"></i>
+                    <i class="bx bx-star icon"></i>
+                    <h1 class="teks5">Semuanya Tiket (02)</h1>
+                    <i class="bx bx-down-arrow iconss"></i>
                 </div>
-                <br /><br /><br />
-                <li class="item">
-                    <a href="{{ route('dashboard.index') }}" class="nav_link submenu_item">
-                        <span class="navlink_icon">
-                            <i class="bx bx-home-alt"></i>
-                        </span>
-                        <span class="navlink">Dashboard</span>
-                        <i class="bx bx-chevron-right arrow-left"></i>
-                    </a>
-                </li>
-                <li class="item">
-                    <div href="#" class="nav_link submenu_item">
-                        <span class="navlink_icon">
-                            <img src="{{ asset('assets/ticket.png') }}" alt="Ticketing" style="width: 24px; height: 24px;" />
-                        </span>
-                        <span class="navlink">Ticketing</span>
-                        <i class="bx bx-chevron-right arrow-left"></i>
+
+                <!-- Card Section -->
+                <div class="card">
+                    <div class="card-header">
+                        <h1 class="batal">Pembatalan Pesanan</h1>
+                        <span class="author">Zulkifli Hartanto</span>
+                        <div class="icon4">
+                            <i class="bx bx-user"></i>
+                        </div>
                     </div>
-                </li>
-            </ul>
+                    <div class="card-body">
+                        <button class="status open">Open</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h1 class="batal">Pembatalan Pesanan</h1>
+                        <span class="author">Zulkifli Hartanto</span>
+                        <div class="icon4">
+                            <i class="bx bx-user"></i>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <button class="status open">Open</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+        <div class="header-content">
+            <i class='bx bx-message-rounded-dots icons1'></i>
+            <h1 class="title">Pembatalan Pesanan</h1>
+            <span class="nama">Zulkifli Hartanto</span>
+            <span class="hinga">-</span>
+            <span class="date">Dibuat pada tanggal 12 Agustus 2024</span>
         </div>
-    </nav>
+        <button class="status1">Open</button>
+        <div class="tab-navigation">
+            <a href="#" class="tab active">Percakapan (5)</a>
+            <a href="#" class="tab">Detail</a>
+            <a href="#" class="tab">Riwayat</a>
+        </div>
 
-    <div class="header">
-        <p>1111</p>
+<div class="chat-container">
+    <div class="date-container">
+        <hr class="date-divider">
+        <span class="date-text">Jan, 19 Sep</span>
+        <hr class="date-divider">
     </div>
+<div class="container-messange">
 
-    <div class="container1">
-        <!-- Konten Anda di sini -->
-    </div>
-    <div class="title">
-        <i class="bx bx-chevron-left"></i>
-        <i class="bx bx-star icon"></i>
-        <h5>SemuanyaTiket(02)</h5>
-        <span class="icon-down">
-            <i class="bx bx-down-arrow"></i>
-        </span>
-    </div>
+</div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 </body>
 </html>
